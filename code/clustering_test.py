716b7eb2ba2a8b2a2c5100b4e64ab5a30342dd0d
Eddy Almuiña <almuina.e@gmail.com>
2018-03-04 23:49:56 -0500

    removed MPEG-7 + spectral features

--- code/clustering_test.py
+++ code/clustering_test.py
@@ -2,47 +2,53 @@ import pathlib
 import time
 
 import essentia
+import essentia.standard as es
 import numpy as np
-from essentia.standard import Windowing, Spectrum, MFCC, MonoLoader, FrameGenerator
 from hdbscan import HDBSCAN
 from sklearn.cluster import KMeans
 
-X = []
-y = []
-w = Windowing(type='hann')
-spectrum = Spectrum()
-mfcc = MFCC()
 
-for file in pathlib.Path('../sounds/testing').iterdir():
-    audio = MonoLoader(filename=str(file))()
-    y.append(file.name)
-    mfccs = []
-    for frame in FrameGenerator(audio, frameSize=1024, hopSize=512, startFromZero=True):
-        spec = spectrum(w(frame))
-        _, mfcc_coeffs = mfcc(spec)
-        mfccs.append(mfcc_coeffs)
-    mfccs = essentia.array(mfccs).T[1:, :]
-    X.append(mfccs.mean(1))
+def main():
+    X = []
+    y = []
+    w = es.Windowing(type='hann')
+    spectrum = es.Spectrum()
+    mfcc = es.MFCC()
 
-X = np.array(X)
+    for file in pathlib.Path('../sounds/testing').iterdir():
+        audio = es.MonoLoader(filename=str(file))()
+        y.append(file.name)
+        mfccs = []
+        for frame in es.FrameGenerator(audio, frameSize=1024, hopSize=512, startFromZero=True):
+            spec = spectrum(w(frame))
+            _, mfcc_coeffs = mfcc(spec)
+            mfccs.append(mfcc_coeffs)
+        mfccs = essentia.array(mfccs).T[1:, :]
+        X.append(mfccs.mean(1))
 
-kmeans = KMeans(n_clusters=4)
-hdbscan = HDBSCAN(min_cluster_size=3, gen_min_span_tree=True)
+    X = np.array(X)
 
-algorithms = [
-    ('KMeans', kmeans),
-    ('HDBSCAN', hdbscan)
-]
+    kmeans = KMeans(n_clusters=4)
+    hdbscan = HDBSCAN(min_cluster_size=3, gen_min_span_tree=True)
 
-for algorithm_name, algorithm in algorithms:
-    start = time.time()
-    algorithm.fit(X)
+    algorithms = [
+        ('KMeans', kmeans),
+        ('HDBSCAN', hdbscan)
+    ]
 
-    results = list(zip(algorithm.labels_, y))
-    results.sort()
-    for label, segment_name in results:
-        print(label, segment_name, sep='\t')
+    for algorithm_name, algorithm in algorithms:
+        start = time.time()
+        algorithm.fit(X)
 
-    print('--------------------')
-    print('done %s in %d seconds' % (algorithm_name, time.time() - start))
-    print('--------------------\n')
+        results = list(zip(algorithm.labels_, y))
+        results.sort()
+        for label, segment_name in results:
+            print(label, segment_name, sep='\t')
+
+        print('--------------------')
+        print('done %s in %d seconds' % (algorithm_name, time.time() - start))
+        print('--------------------\n')
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/features.py
@@ -0,0 +1,79 @@
+import essentia
+import essentia.standard as es
+import pylab as pl
+from matplotlib import collections as mc
+
+
+def plot_temporal_descriptors(audio):
+    awf = []
+    pool = essentia.Pool()
+    power = es.InstantPower()
+
+    for i, frame in enumerate(es.FrameGenerator(audio, frameSize=1024, hopSize=1024, startFromZero=True)):
+        awf.append([(i, frame.min()), (i, frame.max())])
+        pool.add('AP', power(frame))
+
+    awf = essentia.array(awf)
+
+    fig, (ax1, ax2) = pl.subplots(1, 2, figsize=(12, 4))
+    fig.subplots_adjust(left=0.07, right=0.97)
+
+    lc = mc.LineCollection(awf, linewidths=2)
+    ax1.add_collection(lc)
+    ax1.autoscale()
+    ax1.set_title('Audio Waveform (AWF)')
+
+    ax2.plot(pool['AP'])
+    ax2.set_title('Audio Power (AP)')
+
+    fig.show()
+
+
+def plot_spectral_descriptors(audio):
+    pool = essentia.Pool()
+    w = es.Windowing(type='hann')
+    spectrum = es.Spectrum()  # FFT() would return the complex FFT, here we just want the magnitude spectrum
+
+    centroid = es.Centroid()
+    moments = es.CentralMoments()
+    roll_off = es.RollOff()
+    flux = es.Flux()
+    flatness = es.Flatness()
+
+    for frame in es.FrameGenerator(audio, frameSize=1024, hopSize=512, startFromZero=True):
+        spec = spectrum(w(frame))
+        pool.add('SC', centroid(spec))
+        pool.add('SS', moments(spec)[2])
+        pool.add('SRO', roll_off(spec))
+        pool.add('SFX', flux(spec))
+        pool.add('SF', flatness(spec))
+
+    fig, ax = pl.subplots(2, 2, figsize=(12, 8))
+    fig.subplots_adjust(left=0.07, right=0.97)
+
+    ax[0, 0].plot(pool['SC'], label='SC')
+    ax[0, 0].plot(pool['SS'], label='SS')
+    ax[0, 0].legend()
+    ax[0, 0].set_title('Spectral Centroid (SC) and Spectral Spread (SS)')
+
+    ax[0, 1].plot(pool['SRO'])
+    ax[0, 1].set_title('Spectral Roll-off (SRO)')
+
+    ax[1, 0].plot(pool['SFX'])
+    ax[1, 0].set_title('Spectral Flux (SFX)')
+
+    ax[1, 1].plot(pool['SF'])
+    ax[1, 1].set_title('Spectral Flatness (SF)')
+
+    fig.show()
+
+
+def main():
+    audio = es.MonoLoader(filename='../sounds/sheep.wav')()
+
+    plot_temporal_descriptors(audio)
+    plot_spectral_descriptors(audio)
+
+
+if __name__ == '__main__':
+    main()
